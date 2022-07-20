7f3724b745cb6c43e64d0cb164011bd2acff6a15
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2022-07-20 19:01:55 +0100

    Updates mongodb connection string

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./db");
 const router = require("./router");
-const session = require("cookie-session");
+const session = require("express-session");
 const app = express();
 const MongoStore= require("connect-mongo")
 const flash=require("connect-flash")
